c5057db73d4d2af0ab3069d3fdedee3639e1cc00
cpcoats <cpcoats@gmail.com>
2013-12-10 14:08:50 -0500

    reports datecreated and date last modified use prettyd_date

--- ost-blogging.py
+++ ost-blogging.py
@@ -200,7 +200,9 @@ class ShowPost(webapp2.RequestHandler):
 
     template_values = {
       'blog' : blog,
-      'post' : post
+      'post' : post,
+      'date_created' : pretty_date(post.date_created),
+      'date_last_modified' : pretty_date(post.date_last_modified)
     }
 
     template = JINJA_ENVIRONMENT.get_template('post.html')
@@ -239,8 +241,6 @@ class UpdatePost(webapp2.RequestHandler):
 
     #redirect back to homepage
     self.redirect('/p/'+blog_url_title+'/'+post_url_title)
-
-
 
 class does_not_exist(webapp2.RequestHandler):
   def get(self):
@@ -284,3 +284,47 @@ app = webapp2.WSGIApplication([
     (r'/p/(.*)/(.*)', ShowPost),
     (r'/(.*)', does_not_exist)
 ], debug=True)
+
+def pretty_date(time=False):
+    ##http://evaisse.com/post/93417709/python-pretty-date-function
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return  "a minute ago"
+        if second_diff < 3600:
+            return str( second_diff / 60 ) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str( second_diff / 3600 ) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff/7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff/30) + " months ago"
+    return str(day_diff/365) + " years ago"
