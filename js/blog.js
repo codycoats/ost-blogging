a71313964a589200b02206efaed6318c9355deb7
cpcoats <cpcoats@gmail.com>
2013-12-15 14:08:18 -0500

    fixed pagination numbering bug; newer/older buttons disable based on curr page

--- js/blog.js
+++ js/blog.js
@@ -6,30 +6,53 @@ $(function(){
     console.log(total_posts);
 
     var page_index = $('#post-list').data('pageindex');
-    console.log(page_index);
+
+    //if pageindex -1 show all posts
+    if (page_index === -1){
+      console.log("limitPostView - show all");
+      $('.list-group-item').hide().toggle();
+    }
 
     //hide all posts
     $('.list-group-item').hide();
 
-    var post_max = page_index * 10 + 1;
-    var post_min = post_max - 10;
+    var post_begin = (page_index * 10) + 1;
+    var post_end = post_begin + 10;
+
+    console.log("post_begin " + post_begin);
+    console.log("post_end " + post_end);
 
     //show only those within current page range
-    for( var i = post_min; i< post_max; i++)
-    {
-      var curr_post = $('.list-group-item[data-postindex="'+(i+1)+'"]');
+    for( var i = post_begin; i < post_end; i++){
+      var curr_post = $('.list-group-item[data-postindex="'+(i)+'"]');
       curr_post.show();
     }
+
+    //correct prev/next button
+    if (post_begin === 1){
+      $('#post-page-prev').addClass('disabled');
+    }
+    else{
+      $('#post-page-prev').removeClass('disabled');
+    }
+
+    if(post_end >= total_posts){
+     $('#post-page-next').addClass('disabled');
+    }
+    else{
+      $('#post-page-next').removeClass('disabled');
+    }
+
   }
 
-  $('#post-page-prev').bind("click", function(){
+  $('#post-page-prev').on("click", function(){
     var currpage = $('#post-list').data('pageindex');
     var newpage = currpage - 1;
     $('#post-list').data('pageindex', newpage);
     limitPostView();
   });
 
-  $('#post-page-next').bind("click", function(){
+  $('#post-page-next').on("click", function(){
     var currpage = $('#post-list').data('pageindex');
     var newpage = currpage + 1;
     $('#post-list').data('pageindex', newpage);
